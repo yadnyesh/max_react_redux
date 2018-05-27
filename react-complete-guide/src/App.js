52fbd2c1a388027094692334a0fdb8a918623f17
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-05-27 14:29:32 +0530

    Use JSX instead of direct code

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Hi, I'm a react App! </h1>
-    //   </div>
-    // );
-    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!!'));
+    return (
+      <div className="App">
+        <h1>Hi, I'm a react App! </h1>
+      </div>
+    );
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!!'));
   }
 }
 

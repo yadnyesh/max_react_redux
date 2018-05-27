fe1df30711273de164e5965ad2b5100505c7bceb
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-05-27 18:34:26 +0530

    Change State

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,14 +3,28 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { name: 'Max', age:28 },
+      { name: 'Manu', age:29 },
+      { name: 'Stephanie', age:30 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log('Was Clicked')
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a react App! </h1>
         <p> Is this really working? </p>
-        <Person name="Yad" age="37"/>
-        <Person name="Day" age="31"> My Hobbies: Cycling</Person>
-        <Person name="Nes" age="22"/>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My Hobbies: Cycling</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!!'));

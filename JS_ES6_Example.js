60a572f51474f7c496cc4d4d937b599b797ecc94
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-05-27 06:46:06 +0530

    First ES6 Class

---
+++ JS_ES6_Example.js
@@ -0,0 +1,13 @@
+class Person {
+  constructor() {
+    this.name = 'Yadnyesh';
+  }
+
+  printMyName() {
+    console.log(this.name);
+  }
+
+}
+
+const myPerson = new Person();
+myPerson.printMyName();

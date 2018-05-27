32c18f5a4c194a19a8da17e5a5ac5d2851bfcdcb
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-05-27 14:37:17 +0530

    Render custom component - Person

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
@@ -7,6 +8,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a react App! </h1>
         <p> Is this really working? </p>
+        <Person />
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!!'));

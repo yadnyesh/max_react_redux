28e90d6f1b8a5b26a39fd6a40571b6b755a25725
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-05-27 15:11:34 +0530

    Using Props

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a react App! </h1>
         <p> Is this really working? </p>
-        <Person />
+        <Person name="Yad" age="37"/>
+        <Person name="Day" age="31"> My Hobbies: Cycling</Person>
+        <Person name="Nes" age="22"/>
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!!'));

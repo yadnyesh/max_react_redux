9867ed6663af98411b68fe51e1f5fc8320e481bf
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-05-27 18:51:00 +0530

    switchNameHandler.bind

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -12,12 +12,12 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
       this.setState({
         persons : [
           { name: 'Yadnyesh', age:37 },
           { name: 'Manu', age:29 },
-          { name: 'Stephanie', age:30 }
+          { name: newName, age:30 }
         ]
       })
   }
@@ -27,14 +27,14 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a react App! </h1>
         <p> Is this really working? </p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={this.switchNameHandler.bind(this, 'Yad!!')}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}/>
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
-          click={this.switchNameHandler}> My Hobbies: Cycling</Person>
+          click={this.switchNameHandler.bind(this, 'Yad!!!!')}> My Hobbies: Cycling</Person>
         <Person
           name={this.state.persons[2].name}
           age={this.state.persons[2].age}/>

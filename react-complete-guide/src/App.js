9dc6fd5a4ad0404183830c461524485c25a49936
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-05-28 07:32:48 +0530

    Update exact element in array

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -14,14 +14,19 @@ class App extends Component {
     showPersons: true
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons : [
-        { name: 'Yadnyesh', age:37 },
-        { name: event.target.value, age:29 },
-        { name: 'Stephanie', age:30 }
-      ]
-    })
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {...this.state.persons[personIndex]};
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons : persons})
   }
 
   togglePersonsHandler = () => {
@@ -30,7 +35,7 @@ class App extends Component {
   }
 
   deletePersonHandler = (personIndex) => {
-    const persons = [... this.state.persons];
+    const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
     this.setState({persons: persons})
   }
@@ -55,6 +60,7 @@ class App extends Component {
                     name={person.name}
                     age={person.age}
                     key={person.id}
+                    changed={(event) => this.nameChangedHandler(event, person.id)}
                    />
           })}
         </div>

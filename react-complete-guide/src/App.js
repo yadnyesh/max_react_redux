d1f345603b629ffbe4b30f54ef6df8a9a5597f06
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-05-28 07:15:13 +0530

    ES6 syntax correction

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -30,7 +30,7 @@ class App extends Component {
   }
 
   deletePersonHandler = (personIndex) => {
-    const persons = [..., this.state.persons.slice];
+    const persons = [... this.state.persons];
     persons.splice(personIndex, 1);
     this.setState({persons: persons})
   }
